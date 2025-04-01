82bbca064857c77cc1d79418841ce83ae0b0111c
VNFowleri <btgaston1@gmail.com>
2025-04-01 13:05:40 -0400

    Updated About Page again again

--- react_front_end_app/src/pages/About.js
+++ react_front_end_app/src/pages/About.js
@@ -12,7 +12,7 @@ const About = () => {
     >
       <h1>Welcome to VeritasOne</h1>
       <p>
-        Veritas One is a free, personalized electronic health record that follows patients throughout their lives.
+        Veritas One is a comprehensive personalized electronic health record that follows patients throughout their lives.
         It centralizes medical data across systems, breaking down healthcare silos so patients can easily access
         and share their records with any provider—anytime, anywhere in the world.
         The platform puts patients in control of how their information is used—whether for better care coordination,
