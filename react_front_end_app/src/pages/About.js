8f231aa665f834dd96a408beb1fc6f4e58641d8b
VNFowleri <btgaston1@gmail.com>
2025-04-01 12:21:56 -0400

    Updated About Page again

--- react_front_end_app/src/pages/About.js
+++ react_front_end_app/src/pages/About.js
@@ -12,16 +12,15 @@ const About = () => {
     >
       <h1>Welcome to VeritasOne</h1>
       <p>
-        Join us on our journey to redefine the future of healthcare—where your data,
-        your decisions, and your health truly come first.
-      </p>
-      <p>
         Veritas One is a free, longitudinal EHR that centralizes all patient data, bridging
         healthcare silos so patients can seamlessly access and share their records with any provider.
         Our platform puts patients in control of how their information is used—whether simply
         for better care coordination, or optionally for targeted advertising and meaningful
         compensation. By combining patient empowerment, streamlined care, and ethical data
         monetization, we aim to transform the healthcare ecosystem on a global scale.
+      </p>
+         <p>
+        Join us on our journey to redefine the future of healthcare. Better Health Starts With Your Story.
       </p>
     </div>
   );
